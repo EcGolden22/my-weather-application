01f6a0ca900246989aa839fa1894728f2daa0610
EcGolden22 <emcsikos@gmail.com>
2024-08-30 15:31:44 -0400

    added api integration for city and temp value

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 function updateWeatherData(response) {
-  let temperatureElement = document.querySelector = ("#app-temp-value");
+  let temperatureElement = document.querySelector("#app-temp-value");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = math.round(temperature);
+  let cityElement = document.querySelector("#app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function citySearch(city) {
   let apiKey = "433f0fed8cd1a00748o3e6b5ta206078";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(updateWeatherData);
 }
 
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#app-city");
-  cityElement.innerHTML = searchInput.value;
+
   citySearch(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSearch);
+
+citySearch("Philadelphia");
